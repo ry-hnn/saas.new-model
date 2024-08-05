875c26c5ba53abc5fb1026e27fccaee8069fff61
Ryan <rhenne300@gmail.com>
2024-08-05 11:26:24 -0400

    responsive update 1

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const menuButton = document.querySelector(".menu-button");
-let isMenuOpen = false;
+const menuOpen = document.querySelector(".menu-open");
+const menuClose = document.querySelector(".menu-close");
+const menu = document.querySelector(".nav__mobile__links--container");
 
 function menuToggle() {
-    isMenuOpen = !isMenuOpen;
-    console.log(isMenuOpen);
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+        menuOpen.style.display = "none";
+        menuClose.style.display = "block";
+    } else {
+        menu.style.display = "none";
+        menuOpen.style.display = "block";
+        menuClose.style.display = "none";
+    }
 }
